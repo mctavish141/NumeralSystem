2efb6efe39376a7a462345d6763b695b908d45d4
Serhii Kopach <Serega@MacBook-Air.local>
2015-09-13 23:15:38 +0300

    Initial Commit

---
+++ NumeralSystem/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NumeralSystem
+//
+//  Created by Serhii Kopach on 13.09.15.
+//  Copyright (c) 2015 Serhii Kopach. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
